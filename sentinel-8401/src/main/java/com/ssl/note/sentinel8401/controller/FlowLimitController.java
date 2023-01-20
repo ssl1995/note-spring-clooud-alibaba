192b59779c4708b4f8517548d7c2b4b1919f5ed7
songshenglin_i <872912679@qq.com>
2023-01-20 16:36:25 +0800

    sentinel热点规则

--- sentinel-8401/src/main/java/com/ssl/note/sentinel8401/controller/FlowLimitController.java
+++ sentinel-8401/src/main/java/com/ssl/note/sentinel8401/controller/FlowLimitController.java
@@ -7,6 +7,7 @@ import com.ssl.note.sentinel8401.handle.HotHandle;
 import com.ssl.note.sentinel8401.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,18 +33,43 @@ public class FlowLimitController {
     }
 
     /**
-     * 热点规则
+     * 热点规则：命中配置规则
      */
     @GetMapping("/testHotKey")
 //    @SentinelResource(value = "testHotKey", blockHandler = "blockHandle")
     @SentinelResource(value = "testHotKey",
             blockHandlerClass = HotHandle.class,
-            blockHandler = "handlerException1"
-    )
+            blockHandler = "handlerException1")
     public String testHotKey(@RequestParam(value = "hot1", required = false) String hot1,
                              @RequestParam(value = "hot2", required = false) String hot2,
                              @RequestParam(value = "hot3", required = false) String hot3) {
         return "----testHotKey";
+    }
+
+
+    /**
+     * 热点规则：抛出Java异常
+     */
+    @GetMapping("/testHotKeyById/{id}")
+    @SentinelResource(
+            value = "falllback",
+            fallback = "fallbackHandler",
+            exceptionsToIgnore = {ArithmeticException.class}// 指定排除不用sentinel处理的Java异常
+    )
+    public String testHotKey(@PathVariable("id") Long id) {
+        if (id < 3) {
+            return "----testHotKeyById";
+        }
+        throw new NullPointerException("id>=3，抛出异常");
+    }
+
+    /**
+     * 热点规则命中后接受到的异常的fallbackHandler
+     * 必须是public的
+     */
+    public String fallbackHandler(Long id, Throwable exception) {
+        // 这里可以返回一个json
+        return "命中id>=3,抛出空指针异常";
     }
 
     /**
@@ -54,6 +80,16 @@ public class FlowLimitController {
                               String hot2,
                               String hot3, BlockException exception) {
         return "系统繁忙！请稍后再试";
+    }
+
+
+    /**
+     * 持久化配置
+     */
+    @GetMapping("/order/test1")
+    @SentinelResource(value = "test1")
+    public String test1() throws InterruptedException {
+        return "test1 ";
     }
 
 }
