687eb89167f73e9f48c9bc9015c8133d81c0fd76
songshenglin_i <872912679@qq.com>
2023-02-04 23:46:31 +0800

    数据库密码加密

--- seata-stock-8802/src/main/java/com/ssl/note/seata/stock/SeataStock8802Application.java
+++ seata-stock-8802/src/main/java/com/ssl/note/seata/stock/SeataStock8802Application.java
@@ -1,10 +1,16 @@
 package com.ssl.note.seata.stock;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.jasypt.encryption.StringEncryptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableDiscoveryClient
